3c0f0d93e20e423dadbe6c839ddca64e3c0a5d8f
Cav Lemasters <cav.lemasters@geneva.edu>
2018-11-01 20:45:55 -0400

    November 1st Update 2.6

--- resources/views/adminPanel.blade.php
+++ resources/views/adminPanel.blade.php
@@ -8,28 +8,41 @@
     <p><strong> This is example text, </strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <strong> This is example text until we work on the page </strong> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
     <div class="container row">
-        <div class="card" style="width: 18rem; margin: 30px;">
-            <img class="card-img-top" src="https://i2.wp.com/freepngimages.com/wp-content/uploads/2014/04/moviecamera_2.png?w=220" style="height:100%; width:100%;" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Movie Management</h5>
-                <p class="card-text">CRUD Functions for managing your Library of Movies. </p>
-                <a href="{{ route('indexMovie') }}" class="btn btn-dark">List of Movies</a>
-                <a href="{{ route('addMovie') }}" class="btn btn-success">Add a Movie</a>
-                <a href="{{ route('status') }}" class="btn btn-primary">Update Movie</a>
-                <a href="{{ route('status') }}" class="btn btn-danger">Delete a Movie</a>
+
+        <div class="card" style="width: 18rem; margin: 35px;">
+            <div class="card-header">
+                Movie Management
             </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><a href="{{ route('indexMovie') }}" class="#">List of Movies</a></li>
+                <li class="list-group-item"><a href="{{ route('addMovie') }}" class="#">Add a Movie</a></li>
+                <li class="list-group-item"><a href="{{ route('status') }}" class="#">Update Movie</a></li>
+                <li class="list-group-item"><a href="{{ route('status') }}" class="#">Remove a Movie</a></li>
+            </ul>
         </div>
 
-        <div class="card" style="width: 18rem; margin: 30px;">
-            <img class="card-img-top" src="https://i1.wp.com/freepngimages.com/wp-content/uploads/2015/08/Apple-Mac-Computer-vintage.png?resize=624%2C500" style="height:58%; width:100%;" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Kiosk Management</h5>
-                <p class="card-text">CRUD Functions for managing your Kiosk Locations. </p>
-                <a href="{{ route('indexKiosk') }}" class="btn btn-dark">List of all Kiosks</a>
-                <a href="{{ route('addKiosk') }}" class="btn btn-success">Add a Kiosk</a>
-                <a href="{{ route('status') }}" class="btn btn-primary">Update a Kiosk</a>
-                <a href="{{ route('status') }}" class="btn btn-danger">Delete a Kiosk</a>
+        <div class="card" style="width: 18rem; margin: 35px;">
+            <div class="card-header">
+                Kiosk Management
             </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><a href="{{ route('indexKiosk') }}" class="#">List of Kiosks</a></li>
+                <li class="list-group-item"><a href="{{ route('addKiosk') }}" class="#">Add a Kiosk</a></li>
+                <li class="list-group-item"><a href="{{ route('status') }}" class="#">Update Kiosk</a></li>
+                <li class="list-group-item"><a href="{{ route('status') }}" class="#">Remove a Kiosk</a></li>
+            </ul>
+        </div>
+
+        <div class="card" style="width: 18rem; margin: 35px;">
+            <div class="card-header">
+                Inventory Management
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><a href="{{ route('indexKiosk') }}" class="#">List of Inventory</a></li>
+                <li class="list-group-item"><a href="{{ route('addKiosk') }}" class="#">Add Inventory</a></li>
+                <li class="list-group-item"><a href="{{ route('status') }}" class="#">Update Inventory</a></li>
+                <li class="list-group-item"><a href="{{ route('status') }}" class="#">Remove Inventory</a></li>
+            </ul>
         </div>
     </div>
     </div>
--- resources/views/movies/indexMovie.blade.php
+++ resources/views/movies/indexMovie.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <hr>
+        In a perfect world, this would dump the entire list of <strong>Available Movies</strong> into a neat pretty format...
 
+        <div align="right">
+            <a href="{{ route('adminPanel') }}"> Back to Admin Panel </a>
+        </div>
+
+    </div>
 
 @endsection
