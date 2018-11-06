76634bdeb4228b1c79ce5810fc253f83abfcbc12
2tallyork <2tallyork@users.noreply.github.com>
2018-11-06 17:56:16 -0500

    November 1 Commit

--- resources/views/library.blade.php
+++ resources/views/library.blade.php
@@ -8,9 +8,10 @@
     <div>
 
         {{--begin the at for each statement--}}
-
+<div class="grid-container" style="display: grid; grid-template-columns: auto auto auto auto; padding: 10px;">
                 <!-- individual movie card -->
-                    <div style="width: 20%; height: 15%; box-shadow: 10px 10px 25px #787878;">
+
+                    <div class="grid-item" style="width: 100%; height: 100%; padding: 15px;">
                             <div class="card" align="center">
                                 <!-- Card image -->
                                 <div class="view overlay">
@@ -20,7 +21,7 @@
                                     </a>
                                 </div>
                                 <!-- Card content -->
-                                <div class="card-body">
+                                <div class="card-body" style="background-color: #c7ccd6;">
                                     <!-- Title -->
                                     <h4 class="card-title"><a>The Chronicles of Narnia : Part One</a></h4>
                                     <!-- Length & Genre -->
@@ -37,7 +38,266 @@
                                 </div>
                             </div>
                     </div>
+
                     <!-- End Card -->
+
+                    <!-- individual movie card -->
+
+                        <div class="grid-item" style="width: 100%; height: 100%; padding: 15px;">
+                                <div class="card" align="center">
+                                    <!-- Card image -->
+                                    <div class="view overlay">
+                                        <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/1/10/The_Chronicles_of_Narnia_-_The_Lion%2C_the_Witch_and_the_Wardrobe.jpg" alt="Card image cap">
+                                        <a href="#">
+                                            <div class="mask rgba-white-slight"></div>
+                                        </a>
+                                    </div>
+                                    <!-- Card content -->
+                                    <div class="card-body" style="background-color: #c7ccd6;">
+                                        <!-- Title -->
+                                        <h4 class="card-title"><a>The Chronicles of Narnia : Part One</a></h4>
+                                        <!-- Length & Genre -->
+                                        <div>
+                                            <h6>
+                                            <i>135 Min.</i>
+                                            <strong> &nbsp; | &nbsp; </strong>
+                                            <i>Action </i>
+                                            </h6>
+                                        </div>
+
+                                        <!-- Button -->
+                                        <!-- <a href="#" class="btn btn-primary" style="margin: 5px;">Add to Cart</a> -->
+                                    </div>
+                                </div>
+                        </div>
+
+                        <!-- End Card -->
+
+                        <!-- individual movie card -->
+
+                            <div class="grid-item" style="width: 100%; height: 100%; padding: 15px;">
+                                    <div class="card" align="center">
+                                        <!-- Card image -->
+                                        <div class="view overlay">
+                                            <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/1/10/The_Chronicles_of_Narnia_-_The_Lion%2C_the_Witch_and_the_Wardrobe.jpg" alt="Card image cap">
+                                            <a href="#">
+                                                <div class="mask rgba-white-slight"></div>
+                                            </a>
+                                        </div>
+                                        <!-- Card content -->
+                                        <div class="card-body" style="background-color: #c7ccd6;">
+                                            <!-- Title -->
+                                            <h4 class="card-title"><a>The Chronicles of Narnia : Part One</a></h4>
+                                            <!-- Length & Genre -->
+                                            <div>
+                                                <h6>
+                                                <i>135 Min.</i>
+                                                <strong> &nbsp; | &nbsp; </strong>
+                                                <i>Action </i>
+                                                </h6>
+                                            </div>
+
+                                            <!-- Button -->
+                                            <!-- <a href="#" class="btn btn-primary" style="margin: 5px;">Add to Cart</a> -->
+                                        </div>
+                                    </div>
+                            </div>
+
+                            <!-- End Card -->
+
+                            <!-- individual movie card -->
+
+                                <div class="grid-item" style="width: 100%; height: 100%; padding: 15px;">
+                                        <div class="card" align="center">
+                                            <!-- Card image -->
+                                            <div class="view overlay">
+                                                <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/1/10/The_Chronicles_of_Narnia_-_The_Lion%2C_the_Witch_and_the_Wardrobe.jpg" alt="Card image cap">
+                                                <a href="#">
+                                                    <div class="mask rgba-white-slight"></div>
+                                                </a>
+                                            </div>
+                                            <!-- Card content -->
+                                            <div class="card-body" style="background-color: #c7ccd6;">
+                                                <!-- Title -->
+                                                <h4 class="card-title"><a>The Chronicles of Narnia : Part One</a></h4>
+                                                <!-- Length & Genre -->
+                                                <div>
+                                                    <h6>
+                                                    <i>135 Min.</i>
+                                                    <strong> &nbsp; | &nbsp; </strong>
+                                                    <i>Action </i>
+                                                    </h6>
+                                                </div>
+
+                                                <!-- Button -->
+                                                <!-- <a href="#" class="btn btn-primary" style="margin: 5px;">Add to Cart</a> -->
+                                            </div>
+                                        </div>
+                                </div>
+
+                                <!-- End Card -->
+
+                                <!-- individual movie card -->
+
+                                    <div class="grid-item" style="width: 100%; height: 100%; padding: 15px;">
+                                            <div class="card" align="center">
+                                                <!-- Card image -->
+                                                <div class="view overlay">
+                                                    <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/1/10/The_Chronicles_of_Narnia_-_The_Lion%2C_the_Witch_and_the_Wardrobe.jpg" alt="Card image cap">
+                                                    <a href="#">
+                                                        <div class="mask rgba-white-slight"></div>
+                                                    </a>
+                                                </div>
+                                                <!-- Card content -->
+                                                <div class="card-body" style="background-color: #c7ccd6;">
+                                                    <!-- Title -->
+                                                    <h4 class="card-title"><a>The Chronicles of Narnia : Part One</a></h4>
+                                                    <!-- Length & Genre -->
+                                                    <div>
+                                                        <h6>
+                                                        <i>135 Min.</i>
+                                                        <strong> &nbsp; | &nbsp; </strong>
+                                                        <i>Action </i>
+                                                        </h6>
+                                                    </div>
+
+                                                    <!-- Button -->
+                                                    <!-- <a href="#" class="btn btn-primary" style="margin: 5px;">Add to Cart</a> -->
+                                                </div>
+                                            </div>
+                                    </div>
+
+                                    <!-- End Card -->
+
+                                    <!-- individual movie card -->
+
+                                        <div class="grid-item" style="width: 100%; height: 100%; padding: 15px;">
+                                                <div class="card" align="center">
+                                                    <!-- Card image -->
+                                                    <div class="view overlay">
+                                                        <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/1/10/The_Chronicles_of_Narnia_-_The_Lion%2C_the_Witch_and_the_Wardrobe.jpg" alt="Card image cap">
+                                                        <a href="#">
+                                                            <div class="mask rgba-white-slight"></div>
+                                                        </a>
+                                                    </div>
+                                                    <!-- Card content -->
+                                                    <div class="card-body" style="background-color: #c7ccd6;">
+                                                        <!-- Title -->
+                                                        <h4 class="card-title"><a>The Chronicles of Narnia : Part One</a></h4>
+                                                        <!-- Length & Genre -->
+                                                        <div>
+                                                            <h6>
+                                                            <i>135 Min.</i>
+                                                            <strong> &nbsp; | &nbsp; </strong>
+                                                            <i>Action </i>
+                                                            </h6>
+                                                        </div>
+
+                                                        <!-- Button -->
+                                                        <!-- <a href="#" class="btn btn-primary" style="margin: 5px;">Add to Cart</a> -->
+                                                    </div>
+                                                </div>
+                                        </div>
+
+                                        <!-- End Card -->
+
+                                        <!-- individual movie card -->
+
+                                            <div class="grid-item" style="width: 100%; height: 100%; padding: 15px;">
+                                                    <div class="card" align="center">
+                                                        <!-- Card image -->
+                                                        <div class="view overlay">
+                                                            <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/1/10/The_Chronicles_of_Narnia_-_The_Lion%2C_the_Witch_and_the_Wardrobe.jpg" alt="Card image cap">
+                                                            <a href="#">
+                                                                <div class="mask rgba-white-slight"></div>
+                                                            </a>
+                                                        </div>
+                                                        <!-- Card content -->
+                                                        <div class="card-body" style="background-color: #c7ccd6;">
+                                                            <!-- Title -->
+                                                            <h4 class="card-title"><a>The Chronicles of Narnia : Part One</a></h4>
+                                                            <!-- Length & Genre -->
+                                                            <div>
+                                                                <h6>
+                                                                <i>135 Min.</i>
+                                                                <strong> &nbsp; | &nbsp; </strong>
+                                                                <i>Action </i>
+                                                                </h6>
+                                                            </div>
+
+                                                            <!-- Button -->
+                                                            <!-- <a href="#" class="btn btn-primary" style="margin: 5px;">Add to Cart</a> -->
+                                                        </div>
+                                                    </div>
+                                            </div>
+
+                                            <!-- End Card -->
+
+                                            <!-- individual movie card -->
+
+                                                <div class="grid-item" style="width: 100%; height: 100%; padding: 15px;">
+                                                        <div class="card" align="center">
+                                                            <!-- Card image -->
+                                                            <div class="view overlay">
+                                                                <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/1/10/The_Chronicles_of_Narnia_-_The_Lion%2C_the_Witch_and_the_Wardrobe.jpg" alt="Card image cap">
+                                                                <a href="#">
+                                                                    <div class="mask rgba-white-slight"></div>
+                                                                </a>
+                                                            </div>
+                                                            <!-- Card content -->
+                                                            <div class="card-body" style="background-color: #c7ccd6;">
+                                                                <!-- Title -->
+                                                                <h4 class="card-title"><a>The Chronicles of Narnia : Part One</a></h4>
+                                                                <!-- Length & Genre -->
+                                                                <div>
+                                                                    <h6>
+                                                                    <i>135 Min.</i>
+                                                                    <strong> &nbsp; | &nbsp; </strong>
+                                                                    <i>Action </i>
+                                                                    </h6>
+                                                                </div>
+
+                                                                <!-- Button -->
+                                                                <!-- <a href="#" class="btn btn-primary" style="margin: 5px;">Add to Cart</a> -->
+                                                            </div>
+                                                        </div>
+                                                </div>
+
+                                                <!-- End Card -->
+
+                                                <!-- individual movie card -->
+
+                                                    <div class="grid-item" style="width: 100%; height: 100%; padding: 15px;">
+                                                            <div class="card" align="center">
+                                                                <!-- Card image -->
+                                                                <div class="view overlay">
+                                                                    <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/1/10/The_Chronicles_of_Narnia_-_The_Lion%2C_the_Witch_and_the_Wardrobe.jpg" alt="Card image cap">
+                                                                    <a href="#">
+                                                                        <div class="mask rgba-white-slight"></div>
+                                                                    </a>
+                                                                </div>
+                                                                <!-- Card content -->
+                                                                <div class="card-body" style="background-color: #c7ccd6;">
+                                                                    <!-- Title -->
+                                                                    <h4 class="card-title"><a>The Chronicles of Narnia : Part One</a></h4>
+                                                                    <!-- Length & Genre -->
+                                                                    <div>
+                                                                        <h6>
+                                                                        <i>135 Min.</i>
+                                                                        <strong> &nbsp; | &nbsp; </strong>
+                                                                        <i>Action </i>
+                                                                        </h6>
+                                                                    </div>
+
+                                                                    <!-- Button -->
+                                                                    <!-- <a href="#" class="btn btn-primary" style="margin: 5px;">Add to Cart</a> -->
+                                                                </div>
+                                                            </div>
+                                                    </div>
+
+                                                    <!-- End Card -->
+</div>
+
 
             {{--the at end for each would go here--}}
     </div>
