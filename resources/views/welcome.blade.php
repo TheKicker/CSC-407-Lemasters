36a2e2f52513301dc3f73d306a30cd1daffcec83
Cav Lemasters <cav.lemasters@geneva.edu>
2018-11-09 22:16:43 -0500

    November 9th Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,18 @@
 
 @section('content')
 
-    <h2>Welcome Home</h2>
-    <h4> Every Movie, Every Time &copy </h4>
-    <hr>
+    <div>
+            <div align=right><h4>"Madeira is the greatest professor of all time"</h4></div>
+            <div align=right><h5><strong> ― Cav Lemasters</strong></h5></div>
+        <hr>
+    </div>
+
+    <h2>Welcome to the Future of Entertainment </h2>
     <br>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <strong> This is example text until we work on the page </strong>  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Tincidunt dui ut ornare lectus. Nullam non nisi est sit amet facilisis magna etiam tempor. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris. Cras adipiscing enim eu turpis egestas pretium aenean. Tristique senectus et netus et malesuada fames ac turpis. Viverra nam libero justo laoreet.
-    <p> <strong> This is example text until we work on the page </strong> Tristique magna sit amet purus gravida quis blandit turpis. Dictum varius duis at consectetur lorem. Ipsum dolor sit amet consectetur adipiscing elit. Adipiscing bibendum est ultricies integer quis auctor elit. Vel orci porta non pulvinar. Et netus et malesuada fames ac. At risus viverra adipiscing at in tellus integer feugiat. </p>
-    <p>Vitae congue eu consequat ac felis donec. Enim facilisis gravida neque convallis a cras semper auctor neque. Facilisis volutpat est velit egestas dui. Enim diam vulputate ut pharetra sit amet aliquam id. Lacus sed turpis tincidunt id aliquet risus feugiat in ante. <strong> This is example text until we work on the page </strong> </p>
+    <p> &nbsp; Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <strong> This is example text until we work on the page </strong>  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p> &nbsp; Tincidunt dui ut ornare lectus. Nullam non nisi est sit amet facilisis magna etiam tempor. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris. Cras adipiscing enim eu turpis egestas pretium aenean. Tristique senectus et netus et malesuada fames ac turpis. Viverra nam libero justo laoreet.
+    <p> &nbsp; <strong> This is example text until we work on the page </strong> Tristique magna sit amet purus gravida quis blandit turpis. Dictum varius duis at consectetur lorem. Ipsum dolor sit amet consectetur adipiscing elit. Adipiscing bibendum est ultricies integer quis auctor elit. Vel orci porta non pulvinar. Et netus et malesuada fames ac. At risus viverra adipiscing at in tellus integer feugiat. </p>
+    <p> &nbsp; Vitae congue eu consequat ac felis donec. Enim facilisis gravida neque convallis a cras semper auctor neque. Facilisis volutpat est velit egestas dui. Enim diam vulputate ut pharetra sit amet aliquam id. Lacus sed turpis tincidunt id aliquet risus feugiat in ante. <strong> This is example text until we work on the page </strong> </p>
 
     <div>
         <div style="margin-top: 48px;">
