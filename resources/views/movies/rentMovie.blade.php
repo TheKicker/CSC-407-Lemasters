a4c46ced0ae3063b4cebc973d43b520bcf69c062
Cav Lemasters <cav.lemasters@geneva.edu>
2018-11-19 03:27:28 -0500

    November 18th Update

--- resources/views/movies/rentMovie.blade.php
+++ resources/views/movies/rentMovie.blade.php
@@ -6,14 +6,14 @@
     <div class="container">
 
         <h2> Congratulations!</h2>
-        <hr>
-        <div align="center">
+        <div align="center" style="background-color: white; border-style: solid; border-color: black">
 
-            <h4 style="margin: 25px;">Your movie selection is confirmed! Please return within 72 hours from now. </h4>
+            <h4 style="margin: 25px;">Your movie selection of $movie['id'] confirmed! Please return within 72 hours from now. </h4>
 
-            <img src="https://media.giphy.com/media/XreQmk7ETCak0/giphy.gif" alt="Success!!">
+            <img src="https://media.giphy.com/media/8GY3UiUjwKwhO/source.gif" alt="Success!!" style="height: 60%; width: 50%;">
 
             <h4 style="margin: 25px;">
+                <hr>
             <a href="{{route('library')}}">Back to Library</a>
             </h4>
         </div>
