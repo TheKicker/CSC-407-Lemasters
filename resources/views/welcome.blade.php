9dbc86fd32ba69131647cfe0be967482e1c210fe
Cav Lemasters <cav.lemasters@geneva.edu>
2018-12-06 01:02:40 -0500

    December 5th Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,26 +7,41 @@
             <div align=right><h6><strong> ― The Black Knight</strong><i> ( Monty Python and the Holy Grail (1975))</i></h6></div>
         <hr>
     </div>
+<div class="container" style="margin-bottom: 96px;">
 
-    <div>
-        <h2 style="text-shadow: 1px 1px yellow">Welcome to the Future of Entertainment </h2>
-            <br>
-                <p> &nbsp; Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <strong> This is example text until we work on the page </strong>  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p> &nbsp; Tincidunt dui ut ornare lectus. Nullam non nisi est sit amet facilisis magna etiam tempor. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris. Cras adipiscing enim eu turpis egestas pretium aenean. Tristique senectus et netus et malesuada fames ac turpis. Viverra nam libero justo laoreet.
-                <p> &nbsp; <strong> This is example text until we work on the page </strong> Tristique magna sit amet purus gravida quis blandit turpis. Dictum varius duis at consectetur lorem. Ipsum dolor sit amet consectetur adipiscing elit. Adipiscing bibendum est ultricies integer quis auctor elit. Vel orci porta non pulvinar. Et netus et malesuada fames ac. At risus viverra adipiscing at in tellus integer feugiat. </p>
-                <p> &nbsp; Vitae congue eu consequat ac felis donec. Enim facilisis gravida neque convallis a cras semper auctor neque. Facilisis volutpat est velit egestas dui. Enim diam vulputate ut pharetra sit amet aliquam id. Lacus sed turpis tincidunt id aliquet risus feugiat in ante. <strong> This is example text until we work on the page </strong> </p>
-    </div>
-
-    <div>
-
-        <div style="margin-top: 48px;">
-            <h2 style="text-shadow: 1px 1px yellow"> Recommended Titles</h2>
+        <div style="margin-bottom: 36px">
+            <h2 style="text-shadow: 1px 1px yellow">Welcome to the Future of Entertainment </h2>
                 <hr>
-
-                <div align="right">
-
-                <a href="{{ route('library') }}">Browse More ></a>
-                </div>
+                    <p> &nbsp; Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <strong> This is example text until we work on the page </strong>  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p> &nbsp; Tincidunt dui ut ornare lectus. Nullam non nisi est sit amet facilisis magna etiam tempor. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris. Cras adipiscing enim eu turpis egestas pretium aenean. Tristique senectus et netus et malesuada fames ac turpis. Viverra nam libero justo laoreet. </p>
+                    <p> &nbsp; <strong> This is example text until we work on the page </strong> Tristique magna sit amet purus gravida quis blandit turpis. Dictum varius duis at consectetur lorem. Ipsum dolor sit amet consectetur adipiscing elit. Adipiscing bibendum est ultricies integer quis auctor elit. Vel orci porta non pulvinar. Et netus et malesuada fames ac. At risus viverra adipiscing at in tellus integer feugiat. </p>
+                    <p> &nbsp; Vitae congue eu consequat ac felis donec. Enim facilisis gravida neque convallis a cras semper auctor neque. Facilisis volutpat est velit egestas dui. Enim diam vulputate ut pharetra sit amet aliquam id. Lacus sed turpis tincidunt id aliquet risus feugiat in ante. <strong> This is example text until we work on the page </strong> </p>
         </div>
-    </div>
+
+        <div style="margin-bottom: 36px">
+            <h2 style="text-shadow: 1px 1px yellow"> Yellow Bucket Recognized </h2>
+            <hr>
+                <div align="center" style="margin: 24px;">
+                    <img src="https://media.bizj.us/view/img/10143921/boylogogen.jpg">
+                </div>
+
+                    <p> <strong> &nbsp; We are proud to announce that Yellow Bucket finished 1st in Genevas yearly Business of the Year Awards.</strong> Yellow Bucket is owned by Mr. Lemasters, Mrs. Heaton and Mr.York and proudly offers hundreds of popular films sure to make the whole family excited. </p>
+                    <p> &nbsp; Vitae congue eu consequat ac felis donec. Enim facilisis gravida neque convallis a cras semper auctor neque. Facilisis volutpat est velit egestas dui. Enim diam vulputate ut pharetra sit amet aliquam id. Lacus sed turpis tincidunt id aliquet risus feugiat in ante. <strong> This is example text until we work on the page </strong> </p>
+                    <p> &nbsp; Tincidunt dui ut ornare lectus. Nullam non nisi est sit amet facilisis magna etiam tempor. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris. Cras adipiscing enim eu turpis egestas pretium aenean. Tristique senectus et netus et malesuada fames ac turpis. Viverra nam libero justo laoreet. </p>
+
+        </div>
+
+        <div style="margin-bottom: 36px">
+            <h2 style="text-shadow: 1px 1px yellow"> News & Announcements  </h2>
+            <hr>
+            <div align="center" style="margin: 24px;">
+                <img src="https://www.fintechnews.org/wp-content/uploads/2018/08/mundo-1.jpg">
+            </div>
+
+            <p> &nbsp; The President of Yellow Bucket, Mr. Lemasters has announced plans to be the <strong>first streaming service delivered to the International Space Station.  This ground breaking development follows the purchase of Netflix and Red Box by Yellow Bucket. </strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p> &nbsp; We have decided to fire Chief Technical Officer Mrs. Heaton for undisclosed reasons.  Her position will be filled by Mr. Webb who is more than capable of being the best person for the job.  We are happy to have such a great person join us! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <p> We are looking forward to a financially strong and healthy 2019 -- we are excited to ring in the new year with major updates to the platform and expect growth of 12% by Fiscal Year 2020.  </p>
+
+        </div>
+</div>
 @endsection
