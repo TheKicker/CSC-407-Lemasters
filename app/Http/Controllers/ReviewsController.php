c45de475af4a1d6a8375c4285bc8838c456acd05
Cav Lemasters <cav.lemasters@geneva.edu>
2018-12-06 00:05:04 -0500

    December 5th Update

--- app/Http/Controllers/ReviewsController.php
+++ app/Http/Controllers/ReviewsController.php
@@ -8,9 +8,83 @@ use Illuminate\Http\Request;
 
 class ReviewsController extends Controller
 {
-    //
-    public function Create(Movie $movie, Reviews $reviews)
+    /**
+     * Display a listing of the resource.
+     *
+     *
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function create()
     {
         return view('movies.reviewMovie');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Movie $Movie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Movie $Movie)
+    {
+
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Movie $Movie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Movie $Movie)
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Movie $Movie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Movie $Movie)
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Movie $Movie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Movie $Movie)
+    {
+
+
     }
 }
